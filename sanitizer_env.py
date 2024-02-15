45c7a4827001fd08beea490f1c584eb3363f00c0
Your Name <you@example.com>
2024-02-15 16:18:13 +0100

    "Final (non-functioning) version of RL algorithm"

--- sanitizer_env.py
+++ sanitizer_env.py
@@ -1,5 +1,5 @@
 import gymnasium as gym
-from gymnasium.spaces import MultiDiscrete, Dict, Discrete
+from gymnasium.spaces import MultiDiscrete, Dict, Discrete, MultiBinary
 import numpy as np
 import matplotlib
 import copy
@@ -10,10 +10,9 @@ import time
 matplotlib.use('Agg')
 
 class SanitizerWorld(gym.Env):
-   def __init__(self, grid_size=[20, 30], 
+   def __init__(self, grid_size=[10, 15], 
                       render_available =True,
                       seed = 42):
-      
 
       super(SanitizerWorld, self).__init__()
 
@@ -30,15 +29,17 @@ class SanitizerWorld(gym.Env):
       self.map          = np.zeros(self.grid_size)
       self.obstacles    = np.zeros_like(self.map)
       self.energy_level = np.zeros_like(self.map)
-      self.sanitized    = np.zeros_like(self.map)
+      self.sanitized    = np.zeros_like(self.map) 
       self.sum_sanitized      = 0
-      self.reward_threshold   = 2000
-      self.time_step          = 15
+      self.reward_threshold   = 500
+      self.time_step          = 5
       self.san_thresh         = 10*1e-3
 
       # Gen random starting pose in the map  
-      self.current_pos = np.array((np.random.randint(0, self.grid_size[0]-1), 
-                                   np.random.randint(0, self.grid_size[1]-1)))
+      self.current_pos = np.array((np.random.randint(1, self.grid_size[0]-1), 
+                                   np.random.randint(1, self.grid_size[1]-1)))
+      self.current_pos = np.array([1,1])
+      
       self.start_pos       = None
       self.new_pos         = None
       self.prev_direction  = None
@@ -46,15 +47,9 @@ class SanitizerWorld(gym.Env):
 
       # Define action and observation space
       self.action_space = Discrete(4+1)  # Up, Down, Left, Right, DoNothing
-      # TODO: Define observation space
-      # observation and action may vary depends on how we define the problem
-      # observation is a key part of the problem definition because it defines the network as well
-      self.observation_space = MultiDiscrete(np.array([grid_size[0]-1, grid_size[1]-1]))
-      # self.observation_space = Dict(
-      #    {
-      #       "position" : MultiDiscrete(np.array([grid_size[0]-1, grid_size[1]-1])),
-      #       "sanitized": MultiDiscrete(np.ones_like(self.map))
-      #    })
+
+      self.observation_space = gym.spaces.Box(low=-1, high=self.san_thresh, shape=(1, grid_size[0]-2, grid_size[1]-2), dtype=np.float32)
+
       # Initialize the rendering if needed
       if self.render_available:
          self.fig, self.ax = plt.subplots()
@@ -65,8 +60,7 @@ class SanitizerWorld(gym.Env):
          self.ax.set_yticks(np.arange(0,self.map.shape[0]-1,2))
 
          self.image = self.ax.imshow(matrix)
-         # plt.ion()
-         # plt.show()
+
          self.fig.savefig('demo.png', bbox_inches='tight')
 
 
@@ -75,12 +69,15 @@ class SanitizerWorld(gym.Env):
       Reset the environment to the initial state.
       """
       super().reset(seed=seed, options=options)
-      self.start_pos   = np.array((np.random.randint(0, self.grid_size[0]), 
-                                   np.random.randint(0, self.grid_size[1])))
+      self.start_pos   = np.array((np.random.randint(1, self.grid_size[0]-1), 
+                                   np.random.randint(1, self.grid_size[1]-1)))
+      self.start_pos = np.array([1,1])
+   
       self.current_pos = self.start_pos
       self.new_pos = self.current_pos
       self.obstacles = self._generate_obstacles()
       self.sanitized = np.zeros_like(self.map)
+      
       self.sum_sanitized = 0
       self.energy_level = np.zeros_like(self.map)
       obs = self._get_observation()
@@ -98,6 +95,8 @@ class SanitizerWorld(gym.Env):
          self.current_pos = copy.deepcopy(self.new_pos)
          if action != 0:
             self.direction = action
+      else:
+         self.new_pos = copy.deepcopy(self.current_pos)
       self._update_energy_level()
       self.sanitized[self.energy_level > self.san_thresh] = 1
 
@@ -114,10 +113,6 @@ class SanitizerWorld(gym.Env):
 
       obstacles = np.zeros_like(self.map)
 
-      # Generate random obstacles
-      # if random p > 0.05, then obstacle
-      # TODO: (Additional) Implement the logic to more complex obstacles (e.g. walls)
-      #obstacles[np.random.random_sample(obstacles.shape) > 0.95] = 1
       for i in range(self.grid_size[0]):
          obstacles[i,0] = 1
          obstacles[i,-1] = 1
@@ -131,41 +126,21 @@ class SanitizerWorld(gym.Env):
       """
       Update the energy level based on the current position.
       """
-      # TODO: Implement the logic to update the energy level
       P = 100*1e-6
       new_energy_level = np.zeros_like(self.energy_level)
-      for i in range(self.grid_size[0]):
-         for j in range(self.grid_size[1]):
+      for i in range(1,self.grid_size[0]-1):
+         for j in range(1,self.grid_size[1]-1):
             if i == self.current_pos[0] and j == self.current_pos[1]:
                continue
             else:
                E = P*self.time_step / (self.cell_size*((i-self.current_pos[0])**2 + (j-self.current_pos[1])**2))
                new_energy_level[i,j] = E
-      
       self.energy_level = self.energy_level + new_energy_level
-      
 
    def _get_done(self):
-      """
-      Return 'done' signal based on the current position.
-      This can be customized based on the specific done logic.
-      """
-
-      # TODO: Implement the logic to end the episode
-      # the done signal can be defined in various ways:
-      # Max number of steps reached ...
-      # Agent reached the goal ...
-      # Agent is not moving ...
-      # Agent perform wrong movement ...
-
-      # Return randomly true or false
-      # if np.random.random_sample() > 0.99:
-      #    return True
-      print(self.sum_sanitized)
-      print(self.grid_size[0]*self.grid_size[1])
-      print(np.sum(self.obstacles))
-      print('\n')
-      if (self.grid_size[0]*self.grid_size[1] - np.sum(self.obstacles)) == self.sum_sanitized:
+      if (self.sanitized[1:-1, 1:-1].max())>1:
+         print("ERROR: Sanitized map has values greater than 1")
+      if ((self.grid_size[0]-2)*(self.grid_size[1]-2)) <= self.sum_sanitized:
          return True
       else:
          return False
@@ -175,34 +150,31 @@ class SanitizerWorld(gym.Env):
       Get the current observation of the environment.
       This can be customized based on the specific observation logic.
       """
-      # TODO: Implement the logic to shape the observation
-      # Observation defined the network input, so it's critical to define it properly
-      # The observation should contain all the information the agent needs to make a decision
-      # unnecessary information can be removed to reduce the network complexity
-      # obs = {}
-      # obs['position'] = self.current_pos
-      # obs['sanitized'] = self.sanitized
 
-      return self.current_pos
+      # Make observation a copy of the self.sanitized, excluding the borders
+      obs = copy.deepcopy(self.energy_level[1:-1, 1:-1])
+      # Set current position in the map to -1
+      obs[self.current_pos[0]-1, self.current_pos[1]-1] = -1
+      return obs
 
    def _get_reward(self):
       """
       Get the current reward based on the agent's position.
       This can be customized based on the specific reward logic.
       """
-      # TODO: Implement the logic to shape the reward
-      # Reward is the signal that the network is trying to maximize
-      # Reward is a combination of positive and negative (penalizing) signals
-      
-      reward = (np.sum(self.sanitized) - self.sum_sanitized)**2 #Num of new sanitized pixels
-      self.sum_sanitized = np.sum(self.sanitized)
-      reward -= 2 #Penalty for each step
-      if self.prev_direction == self.direction:
-         reward += 10 #Reward for moving in the same direction
+      reward = 0
 
-      if self.new_pos[0] == self.start_pos[0] and self.new_pos[1] == self.start_pos[1]:
-         reward -= 10 #Penalty for not moving
- 
+      # Reward the robot for sanitizing the map (exploration)
+      reward += (np.sum(self.sanitized) - self.sum_sanitized) #Num of new sanitized pixels
+
+      self.sum_sanitized = np.sum(self.sanitized[1:-1, 1:-1])
+
+      # Reward the robot for reaching the goal 
+      if self._get_done():
+         reward += 100
+
+      reward -= 1 #Penalty for each step
+
       return reward
 
    def _get_new_position(self, action):
@@ -210,7 +182,6 @@ class SanitizerWorld(gym.Env):
       Get the new position based on the current position and action.
       """
       # Considering the action as a movement in the grid
-      # TODO: (Additional) Implement the logic to more complex movements (e.g. diagonal)
       new_pos = copy.deepcopy(self.current_pos)
  
       if action == 1:  # Up
@@ -259,9 +230,6 @@ class SanitizerWorld(gym.Env):
       # Make goal position element red
       matrix[self.new_pos[0], self.new_pos[1]] = [1, 0, 0]
       
-      #TODO: Plot the reached energy level
-      ...
-
       return matrix
 
    def render(self):
@@ -283,7 +251,8 @@ class SanitizerWorld(gym.Env):
 
       # Redraw the plot
       self.fig.canvas.draw()
-
+   
       #plt.pause(0.1)
       self.fig.savefig('demo.png', bbox_inches='tight')
       time.sleep(0.1)
+
--- turtle_sanitizer_pytorch_RL.py
+++ turtle_sanitizer_pytorch_RL.py
@@ -9,36 +9,37 @@ import torch.optim as optim
 from torch.distributions import Categorical
 from gymnasium.wrappers import FlattenObservation
 from gymnasium.spaces.utils import flatten
-
+import matplotlib.pyplot as plt
 import sanitizer_env
 
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-print(device)
+
 
 class DQN(nn.Module):
-    def __init__(self, n_obs, n_action):
+    def __init__(self, input_shape, n_action):
         super(DQN, self).__init__()
-        self.l1 = nn.Linear(n_obs, 128)
-        self.l2 = nn.Linear(128, 128)
-        self.l3 = nn.Linear(128, n_action)
-        self.dropout = nn.Dropout(p=0.6) # Used to prevent overfitting
+        self.conv1 = nn.Conv2d(in_channels=input_shape[0], out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(64*input_shape[1]*input_shape[2], 128) 
+        self.fc2 = nn.Linear(128, n_action)
+        self.dropout = nn.Dropout(p=0.5)
         self.saved_log_probs = []
         self.rewards = [1]
-        
 
     def forward(self, x):
-        # Goes through the network and returns the action to take
         x = x.to(device)
-        x = self.l1(x)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = x.view(-1, 64 * x.shape[1]*x.shape[2])
+        x = F.relu(self.fc1(x))
         x = self.dropout(x)
-        x = F.relu(x)
-        x = self.l2(x)
-        x = self.dropout(x)
-        x = F.relu(x)
-        x = self.l3(x)
-        p = F.softmax(x, dim=1)
+        p = F.softmax(self.fc2(x), dim=1)
         return p
+    
     
 class Learner():
     def __init__(self):
@@ -48,29 +49,29 @@ class Learner():
         self.EPS_START = 0.9
         self.EPS_END = 0.05
         self.EPS_DECAY = 1000
-        self.TAU = 0.005
-        self.LR = 1e-5
+        self.TAU = 0.001
+        self.LR = 1e-4
         self.NUM_EPISODES = 1000
-        self.NUM_STEPS = 1000
+        self.NUM_STEPS = 300
         self.LOG_INTERVAL = 10  
         self.EPS = np.finfo(np.float32).eps.item()
         self.PATH = 'dqn_sanitizer.pth'
 
-        #self.env = FlattenObservation(sanitizer_env.SanitizerWorld())
         self.env = sanitizer_env.SanitizerWorld()
-        self.n_obs = self.env.observation_space.shape[0]
+        
+        self.obs_shape = self.env.observation_space.shape
         self.n_action = self.env.action_space.n
-        self.dqn = DQN(self.n_obs, self.n_action)
+        self.dqn = DQN(self.obs_shape, self.n_action)
         self.dqn.to(torch.device(device))
         self.optimizer = optim.Adam(self.dqn.parameters(), lr=self.LR)
         
-
     def select_action(self, state):
         state = torch.from_numpy(state).float().unsqueeze(0)
         state.to(self.device)
         probs = self.dqn(state)
         m = Categorical(probs)
-        action = m.sample()
+        # action = m.sample()
+        action = torch.argmax(probs).unsqueeze(0)
         self.dqn.saved_log_probs.append(m.log_prob(action))
         return action.item()
 
@@ -82,7 +83,7 @@ class Learner():
             R = r + self.GAMMA * R
             returns.appendleft(R)
 
-        returns = torch.tensor(returns)
+        returns = torch.tensor(returns,dtype=torch.float32)
         returns.to(self.device)
         returns = (returns - returns.mean()) / (returns.std() + self.EPS)
         
@@ -101,7 +102,10 @@ class Learner():
 
     def train(self):
         self.env.reset()
-        running_reward = 10
+        running_reward = -200
+        reward_list = []
+        time_step_list = []
+        avg_reward_list = []
         for n in range(self.NUM_EPISODES):
             obs = self.env.reset()
             ep_reward = 0
@@ -112,31 +116,54 @@ class Learner():
                 ep_reward += reward
                 if done:
                     self.dqn.rewards.append(0) #To prevent nan in loss function
+                    time_step_list.append(t)
                     break
 
+            reward_list.append(ep_reward)
+
             running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
+            avg_reward_list.append(running_reward)
             self.params_update()
             if n % self.LOG_INTERVAL == 0:
                 print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
                     n, ep_reward, running_reward))
             
             if running_reward > self.env.reward_threshold:
-                print(f"Reward exced the threhsold! Running reward is now {running_reward}, last episode duration: {t} time steps!")
+                print(f"Reward exceeded the threshold! Running reward is now {running_reward}, last episode duration: {t} time steps!")
                 break
+        
+        # Save image of the reward list
+        plt.figure()
+        plt.plot(reward_list)
+        plt.ylabel('Reward')
+        plt.xlabel('Episode')
+        plt.savefig('learning_curve.png')
+        # Save image of the time step list
+        plt.figure()
+        plt.plot(time_step_list)
+        plt.ylabel('Time Steps')
+        plt.xlabel('Episode')
+        plt.savefig('time_steps_curve.png')
+        # Save image of the average reward list
+        plt.figure()
+        plt.plot(avg_reward_list)
+        plt.ylabel('Average Reward')
+        plt.xlabel('Episode')
+        plt.savefig('average_reward_curve.png')
+        
         torch.save(self.dqn, self.PATH)
 
 
     def test(self):     
         total_reward = 0
         num_eval_episodes = 10
-        self.dqn = DQN(self.n_obs, self.n_action)
+        self.dqn = DQN(self.obs_shape, self.n_action)
         self.dqn.to(torch.device(device))
-        #self.dqn.load_state_dict(torch.load(self.PATH))
-        self.qdn = torch.load(self.PATH)
+        self.dqn = torch.load(self.PATH, map_location=torch.device(device))
         self.dqn.eval()
 
         input('Enter to start testing....')
-        
+    
         for _ in range(num_eval_episodes):
             obs = self.env.reset()
             print('New episode')
@@ -151,7 +178,7 @@ class Learner():
 
 def main():
     learner = Learner()
-    #learner.train()
+    learner.train()
     learner.test()
 
 main()
